0f520255f0d828cbe9dc18164b4dcecb182cad52
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2016-11-09 16:33:03 +0000

    Further development of haptic interface

--- haptic_interface/HapticHandler.py
+++ haptic_interface/HapticHandler.py
@@ -16,130 +16,269 @@
     #if _haptic_handler is None:
         #_haptic_handler = HapticHandler(session)
     #return _haptic_handler
-
+import ctypes
+import numpy
 class HapticHandler():
-    import ctypes
-    _HapticHandler = ctypes.CDLL('./HapticHandler.so')
+    _HapticHandler = ctypes.CDLL('/home/tic20/chimerax_start/haptic_interface/_HapticHandler.so')
     
     def __init__ (self, session):
         self.session = session
+        self.log = session.logger.info
+        self._MAX_DEVICES = 16
+        
+        self._running = False
+        
+        # Handler for arrow models
+        self._arrow_model = [None] * self._MAX_DEVICES
+        
+        # Event handler for display update
+        self._event_handler = None
+        
+        self._arrow_radius = 0.10
+        self._arrow_aspect_ratio = 10
+        
+        from chimerax.core.geometry import place
+        # Arrow rotation matrix
+        self._arrow_rotation = place.rotation((0,1,0), -90)
+        
+        # Mapping of device axes to scene axes and vice versa
+        self._axis_order = numpy.array([0, 2, 1])
+        
+        # Direction of device axes relative to scene axes and vice versa
+        self._axis_directions = numpy.array([1, 1, -1])
+        
+        # Current scaling factor between device and scene coordinates
+        self._final_axis_scale = None
+        
+        
+        self._final_axes_and_origin = None
+        
+        # Axes in the reference frame of the camera
+        self._display_axes_and_origin = None
+        
+        # Scaling factor relating interface coordinates to display coordinates
+        self.display_scaling_factor = 110.0
+        
+        # Scaling factor after taking into account current pixel size.
+        # Used to maintain constant arrow size, and to scale spring constants.
+        self._final_display_scale = None
                 
         # Number of connected devices detected by the Chai3D library
         self.numHapticDevices = 0
         
         # Current (x,y,z) position of each device
-        self.hapticDevicePosition = []
+        self.hapticDevicePosition = [None] * self._MAX_DEVICES
         
         # Target (x,y,z) position of each device
-        self.targetPosition = []
+        self.targetPosition = [None] * self._MAX_DEVICES
         
-        # Spring constant for each device
-        self.springConstant = []
+        # Spring constant for each device, with length units in Angstroms
+        self.springConstant = [50] * self._MAX_DEVICES
         
         # Turn damping on/off for each device
-        self.useDamping = []
+        self.useDamping = [True] * self._MAX_DEVICES
         
         # Turn force feedback on/off for each device
-        self.useFeedback = []
+        self.useFeedback = [True] * self._MAX_DEVICES
         
         # Is the device attached to something?
-        self.deviceInUse = []
+        self.deviceInUse = [False] * self._MAX_DEVICES
         
+        ####
+        # functions defined in _HapticHandler.so
+        ####
     
-    self._startHaptics = self._HapticHandler.startHaptics
-    
+        self._startHaptics = self._HapticHandler.startHaptics
+        
+        self._stopHaptics = self._HapticHandler.stopHaptics
+
+        self._getNumDevices = self._HapticHandler.getNumDevices
+        self._getNumDevices.restype = ctypes.c_int
+        
+        self._setSpringConstant = self._HapticHandler.setSpringConstant
+        self._setSpringConstant.argtypes = [ctypes.c_int, ctypes.c_double]
+            
+        self._setDamping = self._HapticHandler.setDamping
+        self._setDamping.argtypes = [ctypes.c_int, ctypes.c_bool]
+
+        self._setTargetPosition = self._HapticHandler.setTargetPosition
+        self._setTargetPosition.argtypes = [ctypes.c_int, 
+                ctypes.c_double, ctypes.c_double, ctypes.c_double]
+
+        self._startTugging = self._HapticHandler.startTugging
+        self._startTugging.argtypes = [ctypes.c_int]
+
+        self._stopTugging = self._HapticHandler.stopTugging
+        self._stopTugging.argtypes = [ctypes.c_int]
+
+        self._turnOnFeedback = self._HapticHandler.turnOnFeedback
+        self._turnOnFeedback.argtypes = [ctypes.c_int]
+        
+        self._turnOffFeedback = self._HapticHandler.turnOffFeedback
+        self._turnOffFeedback.argtypes = [ctypes.c_int]
+         
+        self._getPosition = self._HapticHandler.getPosition
+        self._getPosition.argtypes = [ctypes.c_int]
+        self._getPosition.restype = ctypes.POINTER(ctypes.c_double * 3)
+             
+        self._getButtonStates = self._HapticHandler.getButtonStates
+        self._getButtonStates.argtypes = [ctypes.c_int]
+        self._getButtonStates.restype = ctypes.POINTER(ctypes.c_bool * 4)
+         
+         
     # Initialise all connected haptic devices
     def startHaptics(self):
-        self._startHaptics()
+        if not self._running:
+            self._startHaptics()
+            self._event_handler = self.session.triggers.add_handler('new frame', self.on_refresh)
+        self._running = True
+        self.getNumDevices()
     
-    self._stopHaptics = self._HapticHandler.stopHaptics
     
     # Stop the haptic device handling process
     def stopHaptics(self):
-        self._stopHaptics()
+        if self._running:
+            self._stopHaptics()
+            self.session.triggers.remove_handler(self._event_handler)
+            for i in range(self.numHapticDevices):
+                a = self._arrow_model[i]
+                if a is not None:
+                    self.session.models.close([a])
+                
+        self._running = False
         
-    self._getNumDevices = self._HapticHandler.getNumDevices
-    self._getNumDevices.restype = ctypes.c_int
     
     # Get the number of detected haptic devices
-    def getNumDevices():
-        n = self.numHapticDevices = self._getNumDevices()
-        return n
+    def getNumDevices(self):
+        if self._running:
+            n = self.numHapticDevices = self._getNumDevices()
+            return n
     
-    self._setSpringConstant = self._HapticHandler.setSpringConstant
-    self._setSpringConstant.argtypes = [ctypes.c_int, ctypes.c_double]
     
-    # Set the spring constant for device i
-    def setSpringConstant(i, k):
-        self.springConstant[i] = k
-        self._setSpringConstant(i, k)
+    # Set the spring constant for device i. Length units in Angstroms.
+    # Needs to be scaled to the device length units, otherwise feedback
+    # strength will change as we zoom.
+    def setSpringConstant(self, i, k):
+        if self._running:
+            self.springConstant[i] = k
+            self._setSpringConstant(i, k*self._final_display_scale)
     
-    self._setDamping = self._HapticHandler.setDamping
-    self._setDamping.argtypes = [ctypes.c_int, ctypes.c_bool]
     
     # Turn damping on/off for device i
-    def setDamping(i, d):
-        self.useDamping[i] = d
-        self._setDamping(i, d)
+    def setDamping(self, i, d):
+        if self._running:
+            self.useDamping[i] = d
+            self._setDamping(i, d)
     
-    self._setTargetPosition = self._HapticHandler.setTargetPosition
-    self._setTargetPosition.argtypes = [ctypes.c_int, 
-            ctypes.c_double, ctypes.c_double, ctypes.c_double]
     
-    # Set the target (x, y, z) position for device i    
-    def setTargetPosition(i, x, y, z):
-        self.targetPosition[i] = [x, y, z]
-        self._setTargetPosition(i, x, y, z)
+    # Set the target (x, y, z) position for device i, in device or
+    # scene coordinates  
+    def setTargetPosition(self, i, x, y, z, scene_coords = False):
+        if self._running:
+            if scene_coords:
+                from chimerax.core.geometry import place
+                pos_in_scene = numpy.array([x,y,z])
+                p1 = place.product([self._final_axes_and_origin.inverse(),pos_in_scene])
+                p = (p1/self._final_axis_scale * self._axis_directions)[self._axis_order]
+            else:
+                p = numpy.array([x,y,z])
+            self.targetPosition[i] = p
+            self._setTargetPosition(i, *p)
     
-    self._startTugging = self._HapticHandler.startTugging
-    self._startTugging.argtypes = [ctypes.c_int]
     
     # Start tugging an object with device i
-    def startTugging(i):
-        self.deviceInUse[i] = True
-        self._startTugging(i)
+    def startTugging(self, i):
+        if self._running:
+            self.deviceInUse[i] = True
+            self._startTugging(i)
     
-    self._stopTugging = self._HapticHandler.stopTugging
-    self._stopTugging.argtypes = [ctypes.c_int]
     
     # Stop tugging an object with device i
-    def stopTugging(i):
-        self.deviceInUse[i] = False
-        self._stopTugging(i)
+    def stopTugging(self, i):
+        if self._running:
+            self.deviceInUse[i] = False
+            self._stopTugging(i)
         
-    self._turnOnFeedback = self._HapticHandler.turnOnFeedback
-    self._turnOnFeedback.argtypes = [ctypes.c_int]
     
     # Turn on force feedback for device i
-    def turnOnFeedback(i):
-        self.useFeedback[i] = True
-        self._turnOnFeedback(i)
+    def turnOnFeedback(self, i):
+        if self._running:
+            self.useFeedback[i] = True
+            self._turnOnFeedback(i)
     
-    self._turnOffFeedback = self._HapticHandler.turnOffFeedback
-    self._turnOffFeedback.argtypes = [ctypes.c_int]
-    
+   
     # Turn off force feedback for device i
-    def turnOffFeedback(i):
-        self.useFeedback[i] = False
-        self._turnOffFeedback(i)
+    def turnOffFeedback(self, i):
+        if self._running:
+            self.useFeedback[i] = False
+            self._turnOffFeedback(i)
         
-    self._getPosition = self._HapticHandler.getPosition
-    self._getPosition.argtypes = [ctypes.c_int]
-    self._getPosition.restype = ctypes.c_double * 3
     
     # Get the current position of device i
-    def getPosition(i):
-        return self._getPosition(i)
+    def getPosition(self, i, scene_coords = False):
+        if self._running:
+            if scene_coords:
+                return self._arrow_model[i].position.origin()
+            return [i for i in self._getPosition(i).contents]
     
-    self._getButtonStates = self._HapticHandler.getButtonStates
-    self._getButtonStates.argtypes = [ctypes.c_int]
-    self._getButtonStates.restype = ctypes.c_bool * 4
+    
     
     # Get the states of the buttons on device i
-    def getButtonStates(i):
-        return self._getButtonStates(i)
+    def getButtonStates(self, i):
+        if self._running:
+            buttons = [b for b in self._getButtonStates(i).contents]
+            return buttons
     
+    def on_refresh(self, *_):
+        display_axes_and_origin, axis_scale = self.get_haptic_reference_frame()
+        for i in range(self.numHapticDevices):
+            pos = self.getPosition(i)
+            self._draw_arrow(i, pos, display_axes_and_origin, axis_scale)
+            if self.deviceInUse[i]:
+                self._setSpringConstant(i, self.springConstant[i]*self._final_display_scale)
+
+    def get_haptic_reference_frame(self):
+        s = self.session
+        v = s.main_view
+        c = v.camera
+        camera_place = c.get_position()
+        camera_origin = camera_place.origin()
+        camera_view_direction = c.view_direction()
+        camera_axes = camera_place.axes()
+        window_scale = max(v.window_size) / 1000
+        near_far = v.near_far_distances(c, 0)
+        origin = (near_far[0] + near_far[1])/2 * camera_view_direction + camera_origin
+        from chimerax.core.geometry import place
+        p = place.Place(axes = camera_axes, origin = origin)
+        final_display_scale = self._final_display_scale = self.display_scaling_factor * v.pixel_size()
+        final_axis_scale = self._final_axis_scale = self.display_scaling_factor * window_scale
+        arrow_transform = self._final_axes_and_origin = place.product([p, self._arrow_rotation, place.scale(final_display_scale)])
+        return arrow_transform, final_axis_scale
         
-    
+        
+        
+        
+    def _draw_arrow(self, i, pos, reference_frame, axis_scale = 1.0):
+        import numpy
+        pos = numpy.array(pos)[self._axis_order] * self._axis_directions
+        a = self._arrow_model[i]
+        if a is None or a.deleted:
+            from chimerax.core.models import Model
+            s = self.session
+            self._arrow_model[i] = a = Model('Haptic tool ' + str(i), s)
+            from chimerax.core.surface import custom_cone_geometry
+            a.vertices, a.normals, a.triangles  = custom_cone_geometry(
+                    radius = self._arrow_radius,
+                    height = self._arrow_radius * self._arrow_aspect_ratio,
+                    nc = 20)
+            a.color = (0,255,0,255)
+            s.models.add([a])
+        # Scale and rotate prototype cylinder.
+        from chimerax.core.geometry import place
+        r = reference_frame
+        r = place.product([r, place.translation(axis_scale*pos)])
+        a.position = r
+
+        
         
     
