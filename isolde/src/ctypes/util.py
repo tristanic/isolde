dbbf0f9c52d884b1fdb7bfe0f4b8127b280825de
Tristan Croll <tic20@dhcp-10-248-201-102.eduroam.wireless.private.cam.ac.uk>
2018-04-13 11:19:46 +0100

    Updated convert_and_sanitize_numpy_array() to handle non-numpy iterables

--- isolde/src/ctypes/util.py
+++ isolde/src/ctypes/util.py
@@ -4,7 +4,8 @@ def convert_and_sanitize_numpy_array(array, dtype):
     '''
     Ensure a numpy array has the specified data type and is C-contiguous in memory.
     '''
-    if array.dtype == dtype and array.data.c_contiguous:
+    if isinstance(array, numpy.ndarray) and array.dtype == dtype \
+                                        and array.data.c_contiguous:
         return array
     ret = numpy.empty(array.shape, dtype)
     ret[:] = array
