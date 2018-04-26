483ed21ff8419d255c41097e89289e24dcf443ec
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-04-26 18:14:00 +0100

    Switched to Cambridge Enterprise license

--- isolde/src/molc.cpp
+++ isolde/src/molc.cpp
@@ -1,11 +1,11 @@
 /**
- * @Author: Tristan Croll
- * @Date:   10-Apr-2018
+ * @Author: Tristan Croll <tic20>
+ * @Date:   24-Apr-2018
  * @Email:  tic20@cam.ac.uk
  * @Last modified by:   tic20
- * @Last modified time: 24-Apr-2018
- * @License: Creative Commons BY-NC-SA 3.0, https://creativecommons.org/licenses/by-nc-sa/3.0/.
- * @Copyright: Copyright 2017-2018 Tristan Croll
+ * @Last modified time: 26-Apr-2018
+ * @License: Free for non-commercial use (see license.pdf)
+ * @Copyright: 2017-2018 Tristan Croll
  */
 
 
@@ -21,39 +21,26 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>      // use PyArray_*(), NPY_*
 
-#include <atomstruct/Atom.h>
-#include <atomstruct/AtomicStructure.h>
-#include <atomstruct/Bond.h>
-#include <atomstruct/Chain.h>
-#include <atomstruct/ChangeTracker.h>
-#include <atomstruct/CoordSet.h>
-#include <atomstruct/connect.h>
-#include <atomstruct/destruct.h>     // Use DestructionObserver
-#include <atomstruct/MolResId.h>
-#include <atomstruct/PBGroup.h>
-#include <atomstruct/polymer.h>
-#include <atomstruct/Pseudobond.h>
-#include <atomstruct/PBGroup.h>
-#include <atomstruct/Residue.h>
-#include <atomstruct/RibbonXSection.h>
-#include <atomstruct/Ring.h>
-#include <atomstruct/seq_assoc.h>
-#include <atomstruct/Sequence.h>
+// #include <atomstruct/Atom.h>
+// #include <atomstruct/AtomicStructure.h>
+// #include <atomstruct/Bond.h>
+// #include <atomstruct/Chain.h>
+// #include <atomstruct/ChangeTracker.h>
+// #include <atomstruct/CoordSet.h>
+// #include <atomstruct/connect.h>
+// #include <atomstruct/destruct.h>     // Use DestructionObserver
+// #include <atomstruct/MolResId.h>
+// #include <atomstruct/PBGroup.h>
+// #include <atomstruct/polymer.h>
+// #include <atomstruct/Pseudobond.h>
+// #include <atomstruct/PBGroup.h>
+// #include <atomstruct/Residue.h>
+// #include <atomstruct/RibbonXSection.h>
+// #include <atomstruct/Ring.h>
+// #include <atomstruct/seq_assoc.h>
+// #include <atomstruct/Sequence.h>
 #include <arrays/pythonarray.h>           // Use python_voidp_array()
 #include <pysupport/convert.h>     // Use cset_of_chars_to_pyset
-
-//#include "atomic_cpp/dihedral.h"
-//#include "atomic_cpp/dihedral_mgr.h"
-//#include "geometry/geometry.h"
-//#include "interpolation/nd_interp.h"
-//#include "validation/rama.h"
-//#include "validation/rota.h"
-//#include "restraints_cpp/changetracker.h"
-//#include "restraints_cpp/position_restraints.h"
-//#include "restraints_cpp/distance_restraints.h"
-//#include "restraints_cpp/dihedral_restraints.h"
-//#include "restraints_cpp/rotamer_restraints.h"
-//#include "restraints_cpp/mdff.h"
 
 
 #include <functional>
--- isolde/src/restraints_cpp/mdff.h
+++ isolde/src/restraints_cpp/mdff.h
@@ -1,11 +1,11 @@
 /**
- * @Author: Tristan Croll
- * @Date:   23-Mar-2018
+ * @Author: Tristan Croll <tic20>
+ * @Date:   18-Apr-2018
  * @Email:  tic20@cam.ac.uk
- * @Last modified by:   Tristan Croll
- * @Last modified time: 18-Apr-2018
- * @License: Creative Commons BY-NC-SA 3.0, https://creativecommons.org/licenses/by-nc-sa/3.0/.
- * @Copyright: Copyright 2017-2018 Tristan Croll
+ * @Last modified by:   tic20
+ * @Last modified time: 26-Apr-2018
+ * @License: Free for non-commercial use (see license.pdf)
+ * @Copyright: 2017-2018 Tristan Croll
  */
 
 
