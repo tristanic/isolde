ce5a75b781b871f582081fc4d364f23d4049cc5a
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-04-30 17:48:05 +0100

    Removed superfluous #includes from molc.cpp

--- isolde/src/molc.cpp
+++ isolde/src/molc.cpp
@@ -3,7 +3,7 @@
  * @Date:   24-Apr-2018
  * @Email:  tic20@cam.ac.uk
  * @Last modified by:   tic20
- * @Last modified time: 26-Apr-2018
+ * @Last modified time: 30-Apr-2018
  * @License: Free for non-commercial use (see license.pdf)
  * @Copyright: 2017-2018 Tristan Croll
  */
@@ -21,37 +21,8 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>      // use PyArray_*(), NPY_*
 
-// #include <atomstruct/Atom.h>
-// #include <atomstruct/AtomicStructure.h>
-// #include <atomstruct/Bond.h>
-// #include <atomstruct/Chain.h>
-// #include <atomstruct/ChangeTracker.h>
-// #include <atomstruct/CoordSet.h>
-// #include <atomstruct/connect.h>
-// #include <atomstruct/destruct.h>     // Use DestructionObserver
-// #include <atomstruct/MolResId.h>
-// #include <atomstruct/PBGroup.h>
-// #include <atomstruct/polymer.h>
-// #include <atomstruct/Pseudobond.h>
-// #include <atomstruct/PBGroup.h>
-// #include <atomstruct/Residue.h>
-// #include <atomstruct/RibbonXSection.h>
-// #include <atomstruct/Ring.h>
-// #include <atomstruct/seq_assoc.h>
-// #include <atomstruct/Sequence.h>
 #include <arrays/pythonarray.h>           // Use python_voidp_array()
 #include <pysupport/convert.h>     // Use cset_of_chars_to_pyset
-
-
-#include <functional>
-#include <map>
-#include <set>
-#include <sstream>
-#include <stdexcept>
-#include <stdint.h>
-#include <string>
-#include <vector>
-#include <cmath>
 
 #include "molc.h"
 
