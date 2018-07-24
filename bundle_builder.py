9773c5dcce03fe2912d2b8d47ccf0f4ab649e6a6
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-07-24 17:10:11 +0100

    Attempted monkey-patch to bundle_builder.py for parallel _CModule compile

--- bundle_builder.py
+++ bundle_builder.py
@@ -1,5 +1,31 @@
 # vim: set expandtab ts=4 sw=4:
 
+# Monkey patch to make _CModule compile in parallel
+def _parallelCCompile(self, sources, output_dir=None, macros=None,
+            include_dirs=None, debug=0, extra_preargs=None,
+            extra_postargs=None, depends=None):
+    # those lines are copied from distutils.ccompiler.CCompiler directly
+    macros, objects, extra_postargs, pp_opts, build = self._setup_compile(output_dir, macros, include_dirs, sources, depends, extra_postargs)
+    cc_args = self._get_cc_args(pp_opts, debug, extra_preargs)
+    from concurrent.futures import ThreadPoolExecutor
+    import os
+    results = []
+    with ThreadPoolExecutor(max_workers=os.cpu_count()-1) as executor:
+        print("using ThreadPoolExecutor")
+        for o in objects:
+            src, ext = build[obj]
+            results.append(
+                executor.submit(
+                    self._compile(obj, src, ext, cc_args, extra_postargs, pp_opts)
+                )
+            )
+        for r in results:
+            r.result()
+    return objects
+import distutils.ccompiler
+distutils.ccompiler.CCompiler.compile = _parallelCCompile
+
+
 def distlib_hack(_func):
     # This hack is needed because distlib and wheel do not yet
     # agree on the metadata file name.
@@ -526,6 +552,9 @@ class _CompiledCode:
         inc = bundle.include_dir()
         lib = bundle.library_dir()
         return inc, lib
+
+
+
 
 
 class _CModule(_CompiledCode):
