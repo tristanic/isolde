04a43107aec541beb1fa7022b67b740a6c221e56
Tristan Croll <tic20@cam.ac.uk>
2018-09-27 11:02:51 +0100

    Improved backrub implementation

--- isolde/src/refine/backrub_rotamer.py
+++ isolde/src/refine/backrub_rotamer.py
@@ -4,8 +4,11 @@ class Backrub:
     Automatically fit a protein sidechain into density using the
     Backrub algorithm (Davis et al. 2006, Structure 14: 265-274)
     '''
-    def __init__(self, residue, density_map):
+    def __init__(self, residue, density_map, clash_weight = 0.1):
+        self.session = residue.session
         dm = self._density_map = density_map
+        self._clash_weight = clash_weight
+        self._model = residue.structure
         # Make sure the map is covering the selected residue
         from chimerax.atomic import Residue
         if residue.polymer_type != Residue.PT_AMINO:
@@ -73,31 +76,32 @@ class Backrub:
         map_range = dm.stats.max - dm.stats.min
         # Store the original coordinates to allow reversion
         self._original_coords = self._all_movable_atoms.coords
+        res_atoms = self._main_res_atoms
+        surrounding_atoms = self._potential_clashes = near_atoms(res_atoms, self._model, 10)
         import numpy
         from scipy.optimize import minimize, minimize_scalar
         # First optimize the rotation about the principal axis to get the best
         # fit for CA and CB
-        res_atoms = self._main_res_atoms
         moving_atoms = self._all_movable_atoms
         check_atoms = res_atoms[numpy.in1d(res_atoms.names, ['CA', 'CB'])]
         original_coords = moving_atoms.coords
         #self.rotate_and_check_fit([0], self.primary_axis, self._primary_axis_center, moving_atoms, original_coords, check_atoms)
         minimize_scalar(self.rotate_and_check_fit, bounds=[-20,20],
-            args=(self.primary_axis, self._primary_axis_center, moving_atoms, original_coords, check_atoms),
+            args=(self.primary_axis, self._primary_axis_center, moving_atoms, original_coords, check_atoms, self._clash_weight),
             )
 
         moving_atoms = self._prev_pep
         check_atoms = self._prev_pep
         original_coords = moving_atoms.coords
         minimize_scalar(self.rotate_and_check_fit, bounds=[-20,20],
-            args=(self.prev_pep_axis, self._prev_pep_axis_center, moving_atoms, original_coords, check_atoms),
+            args=(self.prev_pep_axis, self._prev_pep_axis_center, moving_atoms, original_coords, check_atoms, self._clash_weight),
             )
 
         moving_atoms = self._next_pep
         check_atoms = self._next_pep
         original_coords = moving_atoms.coords
         minimize_scalar(self.rotate_and_check_fit, bounds=[-20,20],
-            args=(self.next_pep_axis, self._next_pep_axis_center, moving_atoms, original_coords, check_atoms),
+            args=(self.next_pep_axis, self._next_pep_axis_center, moving_atoms, original_coords, check_atoms, self._clash_weight),
             )
 
         rotamer = self.rotamer
@@ -108,44 +112,54 @@ class Backrub:
 
         cutoff_frac = 0.9
 
-        from chimerax.atomic import Atoms
-        from math import degrees
-        for i in range(num_chi):
-            chi = rotamer.chi_dihedrals[i]
-            moving_atoms = rotamer.moving_atoms(i)
-            check_atoms = []
-            for b in chi.atoms[2].bonds:
-                check_atoms.extend(b.atoms)
-            check_atoms = Atoms(check_atoms)
-            results = []
-            for t in targets:
-                # rotate all the previously checked chis to this rotamer
-                for j in range(i):
-                    c = rotamer.chi_dihedrals[j]
-                    ma = rotamer.moving_atoms(j)
-                    self.rotate_chi_to(c, ma, t['Angles'][j])
+        # For short sidechains it's best to check each target explicitly. For
+        # long/flexible ones, work up from the base outwards, progressively
+        # picking the subset of targets angles that give the best fit for the
+        # atom(s) directly connected to the rotating bond.
+        if num_chi > 2:
+            from chimerax.atomic import Atoms
+            from math import degrees
+            for i in range(num_chi):
+                chi = rotamer.chi_dihedrals[i]
+                moving_atoms = rotamer.moving_atoms(i)
+                if i < num_chi-1:
+                    check_atoms = []
+                    for b in chi.atoms[2].bonds:
+                        check_atoms.extend(b.atoms)
+                    check_atoms = Atoms(check_atoms)
+                else:
+                    # For the last chi, check all remaining moving atoms
+                    check_atoms = moving_atoms
+                results = []
+                for t in targets:
+                    # rotate all the previously checked chis to this rotamer
+                    for j in range(i):
+                        c = rotamer.chi_dihedrals[j]
+                        ma = rotamer.moving_atoms(j)
+                        self.rotate_chi_to(c, ma, t['Angles'][j])
 
 
-                results.append(self.rotate_chi_and_check_fit(
-                    chi, moving_atoms, t['Angles'][i], check_atoms
-                ))
-            results = numpy.array(results)
-            # print("Results for chi {}: {}".format(i, results))
-            # If there is a wide range of values, select out the top few.
-            # Otherwise, keep them all
-            if max(results) - min(results) > map_range/10:
-                min_val = min(results)
-                # Normalise to the range 0..1, and discard everything more than 20%
-                # away from the minimum
-                results -= min_val
-                results /= max(results)
-                cutoff_val = 0.2
-                new_targets = []
-                for t, r in zip(targets, results):
-                    if r < cutoff_val:
-                        new_targets.append(t)
-                targets = new_targets
-            if len(targets) == 1: break
+                    results.append(self.rotate_chi_and_check_fit(
+                        chi, moving_atoms, t['Angles'][i],
+                        self._clash_weight, check_atoms
+                    ))
+                results = numpy.array(results)
+                # print("Results for chi {}: {}".format(i, results))
+                # If there is a wide range of values, select out the top few.
+                # Otherwise, keep them all
+                if max(results) - min(results) > map_range/10:
+                    min_val = min(results)
+                    # Normalise to the range 0..1, and discard everything more than 20%
+                    # away from the minimum
+                    results -= min_val
+                    results /= max(results)
+                    cutoff_val = 0.2
+                    new_targets = []
+                    for t, r in zip(targets, results):
+                        if r < cutoff_val:
+                            new_targets.append(t)
+                    targets = new_targets
+                if len(targets) == 1: break
 
         # By now we should have narrowed it down to quite a short list
         # (hopefully just one). Now to do some more detailed minimization
@@ -154,6 +168,7 @@ class Backrub:
             ','.join([t['Name'] for t in targets])
         ))
 
+        from math import radians
         final_results = []
         for target in targets:
             t_angles = target['Angles']
@@ -164,12 +179,13 @@ class Backrub:
             # print('Bounds: {}'.format(bounds))
             final_results.append(
                 minimize(self.fine_tune_rotamer, t_angles,
-                args=(rotamer,), bounds=bounds ))
+                args=(rotamer, self._clash_weight), bounds=bounds, method='L-BFGS-B',
+                options={'eps':radians(1)} ))
         #print(len(final_results))
         scores = numpy.array([r.fun for r in final_results])
         # print("Scores: {}".format(scores))
         best = final_results[numpy.argmin(scores)].x
-        self.fine_tune_rotamer(best, rotamer)
+        self.fine_tune_rotamer(best, rotamer, self._clash_weight)
 
 
 
@@ -185,7 +201,8 @@ class Backrub:
         tf = rotation(axis, degrees(target_angle-chi_dihedral.angle), center)
         ma.coords = tf.moved(ma.coords)
 
-    def rotate_chi_and_check_fit(self, chi_dihedral, moving_atoms, target_angle, check_atoms = None):
+    def rotate_chi_and_check_fit(self, chi_dihedral, moving_atoms, target_angle,
+            clash_weight, check_atoms = None):
         '''
         Returns the negative sum of density values at the centre of each of the atoms in
         check_atoms after rotating moving_atoms by angle around axis.
@@ -195,9 +212,10 @@ class Backrub:
         weights = check_atoms.elements.numbers
         self.rotate_chi_to(chi_dihedral, moving_atoms, target_angle)
         result = -sum(self._density_map.interpolated_values(check_atoms.coords)*weights)/sum(weights)
+        result += clash_weight*clash_score(self.session, moving_atoms, self._potential_clashes)
         return result
 
-    def rotate_and_check_fit(self, angle, axis, center, moving_atoms, original_coords, check_atoms):
+    def rotate_and_check_fit(self, angle, axis, center, moving_atoms, original_coords, check_atoms, clash_weight):
         '''
         Returns the negative sum of density values at the centre of each of the atoms in
         check_atoms after rotating moving_atoms by angle around axis.
@@ -213,12 +231,13 @@ class Backrub:
                 ' the edge of the displayed map box. Re-center the map on the'
                 ' residue before trying again')
         result = -sum(dvals*weights)/sum(weights)
-        print("Result for angle {}: {}".format(angle, result))
+        result += clash_weight*clash_score(self.session, check_atoms, self._potential_clashes)
+        # print("Result for angle {}: {}".format(angle, result))
         return result
         #return -sum(dvals)
 
 
-    def fine_tune_rotamer(self, angles, rotamer):
+    def fine_tune_rotamer(self, angles, rotamer, clash_weight):
         check_atoms = rotamer.residue.atoms
         nchi = rotamer.num_chi_dihedrals
         import numpy
@@ -228,7 +247,7 @@ class Backrub:
         rot_angles = numpy.degrees(angles-rotamer.angles)
         for i in range(nchi):
             ma = rotamer.moving_atoms(i)
-            weights[check_atoms.indices(ma)] *= 2**(nchi-i)
+            weights[check_atoms.indices(ma)] *= nchi-i+1
             chi = rotamer.chi_dihedrals[i]
             coords = chi.atoms.coords
             axis = coords[2]-coords[1]
@@ -242,13 +261,45 @@ class Backrub:
                 ' the edge of the displayed map box. Re-center the map on the'
                 ' residue before trying again')
         result = -sum(dvals*weights)/sum(weights)
+        clashscore = clash_weight*clash_score(self.session, check_atoms, self._potential_clashes)
+        print("Density score: {}   Clash score: {}".format(result, clashscore))
+        result += clashscore
+        #print('Fine tune rotamer result: {}'.format(result))
         return result
         # return -sum(self._density_map.interpolated_values(check_atoms.coords))
 
 def _true():
     return True
 
-def test_backrub(session, isolde, selected=False, focus=True):
+def clash_score(session, atoms, surrounds):
+    '''
+    Returns the sum of overlap distances for clashing atoms
+    '''
+    from chimerax.atomic.clashes import clashes
+    clash_dict = clashes.find_clashes(session, atoms, test=surrounds)
+    # Make list of unique clashing atom pairs and their distances
+    seen = set()
+    clash_sum = 0
+    for a1, clashes in clash_dict.items():
+        seen.add(a1)
+        for a2, dist in clashes.items():
+            if a2 in seen:
+                continue
+            clash_sum += dist
+    return clash_sum
+
+
+def near_atoms(atoms, model, distance):
+    from chimerax.core.geometry import find_close_points
+    all_atoms = model.atoms
+    other_atoms = all_atoms.subtract(atoms)
+    other_coords = other_atoms.coords
+    test_coords = atoms.coords
+    a, _ = find_close_points(other_coords, test_coords, distance)
+    return other_atoms.filter(a)
+
+
+def test_backrub(session, isolde, clash_weight=0.1, selected=False, focus=True):
     m = isolde.selected_model
     if selected:
         from chimerax.atomic import selected_atoms
@@ -261,7 +312,8 @@ def test_backrub(session, isolde, selected=False, focus=True):
     from chimerax.clipper.symmetry import get_symmetry_handler
     sh = get_symmetry_handler(m)
     mdff_p = sh.xmapset['MDFF potential']
-    b = Backrub(r, mdff_p)
+    b = Backrub(r, mdff_p, clash_weight=clash_weight)
     from ..delayed_reaction import delayed_reaction
     delayed_reaction(session.triggers, 'new frame', _true, [], _true, b.auto_fit, [])
+    return b
     #b.auto_fit()
