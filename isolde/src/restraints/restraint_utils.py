e0b56031ff3ad18024e626f8db884ecece500a52
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-10-25 19:09:02 +0100

    Simple automatic restraints for small ligands

--- isolde/src/restraints/restraint_utils.py
+++ isolde/src/restraints/restraint_utils.py
@@ -55,3 +55,59 @@ def restrain_ca_distances_to_template(template_residues, restrained_residues,
             dr.spring_constant = spring_constant
             dr.target = distance(query_coord[0], template_coords[ind])
             dr.enabled = True
+
+def restrain_small_ligands(model, distance_cutoff=3.5, heavy_atom_limit=3, spring_constant=500,
+    bond_to_carbon=False):
+    '''
+    Residues with a small number of heavy atoms can be problematic in MDFF if
+    unrestrained, since if knocked out of density they tend to simply keep
+    going. It is best to restrain them with distance restraints to suitable
+    surrounding atoms or, failing that, to their starting positions.
+    '''
+    from chimerax.atomic import Residue, Residues
+    residues = model.residues
+    ligands = residues[residues.polymer_types==Residue.PT_NONE]
+    small_ligands = Residues(
+        [r for r in ligands if len(r.atoms[r.atoms.element_names!='H'])<heavy_atom_limit])
+    from .. import session_extensions as sx
+    drm = sx.get_distance_restraint_mgr(model)
+    prm = sx.get_position_restraint_mgr(model)
+    all_heavy_atoms = model.atoms[model.atoms.element_names!='H']
+    if not bond_to_carbon:
+        all_heavy_atoms = all_heavy_atoms[all_heavy_atoms.element_names != 'C']
+    all_heavy_coords = all_heavy_atoms.coords
+
+    from chimerax.core.geometry import find_close_points, distance
+    for r in small_ligands:
+        r_heavy_atoms = r.atoms[r.atoms.element_names != 'H']
+        if not bond_to_carbon:
+            r_non_carbon_atoms = r_heavy_atoms[r_heavy_atoms.element_names !='C']
+            if not len(r_non_carbon_atoms):
+                # No non-carbon heavy atoms. Apply position restraints
+                prs = prm.get_restraints(r_heavy_atoms)
+                prs.targets = prs.atoms.coords
+                prs.spring_constants = spring_constant
+                prs.enableds = True
+                continue
+            r_heavy_atoms = r_non_carbon_atoms
+        r_indices = all_heavy_atoms.indices(r_heavy_atoms)
+        r_coords = r_heavy_atoms.coords
+        applied_drs = False
+        for ra, ri, rc in zip(r_heavy_atoms, r_indices, r_coords):
+            _, found_i = find_close_points([rc], all_heavy_coords, distance_cutoff)
+            found_i = found_i[found_i!=ri]
+            for fi in found_i:
+                if fi in r_indices:
+                    continue
+                dr = drm.add_restraint(ra, all_heavy_atoms[fi])
+                dr.spring_constant = spring_constant
+                dr.target=distance(rc, all_heavy_coords[fi])
+                dr.enabled = True
+                applied_drs = True
+        if not applied_drs:
+            # Ligand is too far from the model for distance restraints. Use
+            # position restraionts
+            prs = prm.add_restraints(r_heavy_atoms)
+            prs.targets = prs.atoms.coords
+            prs.spring_constants = spring_constant
+            prs.enableds = True
