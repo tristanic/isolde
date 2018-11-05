7a1f69dc9c7b50a1cd9ae23738c18ea852946a16
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-11-05 17:20:32 +0000

    Neater imports in Clipper __init__

--- clipper/src/__init__.py
+++ clipper/src/__init__.py
@@ -9,23 +9,51 @@
 
 
 from .main import *
-#from .crystal import CrystalStructure
-from .clipper_mtz import ReflectionDataContainer
+# General objects
+from .clipper_python import (
+    Cell,
+    Cell_descr,
+    CCP4MTZfile,
+    # CIFfile,
+    Coord_grid,
+    Grid,
+    Grid_range,
+    Grid_sampling,
+    HKL_info,
+    Resolution,
+    RTop_frac,
+    RTop_orth,
+    Spacegroup,
+    Spgr_descr,
+    Symop,
+    # Symops,
+    Unit_Cell,
+    Xmap_float as Xmap,
+    )
 
-# from clipper_python import Coord_grid
-# from clipper_python import CCP4MTZfile, CIFfile, CCP4MAPfile
-# from clipper_python import ABCD, HKL, E_sigE, F_phi, F_sigF_ano, F_sigF, Flag, Flag_bool, \
-#                      I_sigI, Phi_fom
-# from clipper_python import HKL_info, HKL_data_ABCD, \
-#                      HKL_data_E_sigE, \
-#                      HKL_data_Flag, HKL_data_Flag_bool, \
-#                      HKL_data_F_sigF, \
-#                      HKL_data_F_sigF_ano, \
-#                      HKL_data_F_phi, \
-#                      HKL_data_I_sigI, \
-#                      HKL_data_Phi_fom
-# from clipper_python import Grid, Grid_range, Grid_sampling, Cell, \
-#                      Cell_descr, Spacegroup, Spgr_descr, Isymop, RTop_frac, \
-#                      Symop, Symops, RTop_orth, Resolution
-# from clipper_python import Util
-# from clipper_python import warn_test, except_test
+
+# Singular forms of HKL data
+from .clipper_python import Flag, Flag_bool
+from .clipper_python.data32 import (
+    ABCD_float as ABCD,
+    E_sigE_float as E_sigE,
+    F_phi_float as F_phi,
+    F_sigF_float as F_sigF,
+    F_sigF_ano_float as F_sigF_ano,
+    I_sigI_float as I_sigI,
+    Phi_fom_float as Phi_fom,
+    )
+
+# Array forms of HKL data
+from .clipper_python import HKL_data_Flag, HKL_data_Flag_bool
+from .clipper_python.data32 import (
+    HKL_data_ABCD_float as HKL_data_F_ABCD,
+    HKL_data_E_sigE_float as HKL_data_E_sigE,
+    HKL_data_F_phi_float as HKL_data_F_phi,
+    HKL_data_F_sigF_float as HKL_data_F_sigF,
+    HKL_data_F_sigF_ano_float as HKL_data_F_sigF_ano,
+    HKL_data_I_sigI_float as HKL_data_I_sigI,
+    HKL_data_Phi_fom_float as HKL_data_Phi_fom,
+    )
+
+from .clipper_mtz import ReflectionDataContainer
