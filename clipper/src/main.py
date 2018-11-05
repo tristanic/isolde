fd61c687f71c64a307a2cc2e9d1c9f3487b796f1
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-11-05 17:21:41 +0000

    Deleted obsolete code

--- clipper/src/main.py
+++ clipper/src/main.py
@@ -8,52 +8,7 @@
 
 
 
-#from . import clipper_python as clipper
 import numpy
-
-# Override logging of Clipper messages to send them to the ChimeraX log
-# rather than just printing them to the console.
-
-# _clipper_messages = clipper._clipper_messages
-#
-# def _log_clipper(func):
-#   def func_wrapper(*args, **kwargs):
-#     _clipper_messages.clear()
-#     func(*args, **kwargs)
-#     message_string = _clipper_messages.read_and_clear()
-#     if message_string:
-#       session.logger.info("CLIPPER WARNING:")
-#       session.logger.info(message_string)
-#   return func_wrapper
-
-# clipper._clipper.log_clipper = _log_clipper
-
-def add_crystal(session, name):
-  from .crystal import Xtal_Project
-  return Xtal_Project(session, name)
-
-
-
-
-
-def apply_b_factors_to_hydrogens(atom_list):
-    '''
-    When hydrogens are added to a pre-existing structure, their isotropic
-    and/or anisotropic B-factors are not necessarily set. This routine simply
-    sets them to match their adjacent heavy atoms. The input atom_list can
-    be either the hydrogens alone, or the whole structure.
-    '''
-    hydrogens = atom_list.filter(atom_list.element_names == 'H')
-    # Get heavy atoms by taking advantage of the fact that hydrogens are
-    # only ever covalently bonded to a single other atom.
-    for h in hydrogens:
-        b = h.bonds[0]
-        for atom in b.atoms:
-            if atom.element_name != 'H':
-                h.bfactor = atom.bfactor
-                h.aniso_u6 = atom.aniso_u6
-                break
-
 
 def atom_list_from_sel(atom_list):
     '''
@@ -96,69 +51,6 @@ def make_unit_cell(model, xmap, draw = True):
         model.positions = ucp
     return unit_cell_frac_symops
 
-def test_pack_box(model, xmap, size = 50):
-    from chimerax.core.geometry import Place, Places
-    uc = make_unit_cell(model, xmap, draw = False)
-    coord = model.bounds().center()
-    box_size = (numpy.ones(3, numpy.int32)*size)
-    bo = xmap.all_symops_in_box(coord-box_size/2, box_size, uc)
-    p = []
-    for b in bo:
-        p.append(Place(matrix=b.rtop_orth(xmap.cell()).matrix()[0:3,:]))
-    P = Places(p)
-    model.positions = P
-
-    from chimerax.core.models import Drawing, Model
-    from chimerax.surface.shapes import sphere_geometry
-    d = Drawing('box corners')
-    m = Model('box', session)
-    d.set_geometry(*sphere_geometry(80))
-    #d.vertices, d.normals, d.triangles = sphere_geometry(80)
-    from .clipper_python import Coord_orth, Coord_grid
-    box_min = Coord_orth(coord-box_size/2).coord_frac(xmap.cell()).coord_grid(xmap.grid_sampling())
-    box_max = box_min + Coord_grid(*box_size.tolist())
-    minmax = [box_min, box_max]
-    dp = []
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
-                thiscg = Coord_grid([minmax[i].u(), minmax[j].v(), minmax[k].w()])
-                dp.append(Place(origin=thiscg.coord_frac(xmap.grid_sampling()).coord_orth(xmap.cell()).xyz))
-    d.positions = Places(dp)
-    m.add_drawing(d)
-    session.models.add([m])
-
-    return bo
-
-def pack_box(model, xmap, box_origin_xyz, size = 100):
-    box_size_xyz = numpy.ones(3)*size
-    box_origin_xyz = numpy.array(box_origin_xyz)
-    model_bounds = model.bounds().box_corners()
-    bo = xmap.pack_xyz_box(model_bounds, box_origin_xyz, box_size_xyz);
-    from chimerax.core.geometry import Place, Places
-
-    p = []
-    for b in bo:
-        p.append(Place(matrix=b.rtop_orth(xmap.cell()).matrix()[0:3,:]))
-    P = Places(p)
-    model.positions = P
-
-    from chimerax.core.models import Drawing, Model
-    from chimerax.surface.shapes import sphere_geometry
-    d = Drawing('box corners')
-    m = Model('box', session)
-    #d.vertices, d.normals, d.triangles = sphere_geometry(80)
-    d.set_geometry(*sphere_geometry(80))
-    minmax = [box_origin_xyz, box_origin_xyz+box_size_xyz]
-    dp = []
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
-                dp.append(Place(origin=[minmax[i][0],minmax[j][1],minmax[k][2]]))
-    d.positions = Places(dp)
-    m.add_drawing(d)
-    session.models.add([m])
-    return bo
 
 def draw_box(min_corner, max_corner, name='box'):
     from chimerax.core.geometry import Place, Places
@@ -183,9 +75,6 @@ def draw_box(min_corner, max_corner, name='box'):
     m.add_drawing(d)
     session.models.add([m])
     return d
-
-
-
 
 def draw_asu(xmap):
     from chimerax.core.geometry import Place, Places
