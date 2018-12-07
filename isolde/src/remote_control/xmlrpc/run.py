a1244f16c2e97cec7a4580dfc95a3f354d608b56
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-12-07 17:19:53 +0000

    Minor tweak to xmlrpc test code

--- isolde/src/remote_control/xmlrpc/run.py
+++ isolde/src/remote_control/xmlrpc/run.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python
 
-# === UCSF ChimeraX Copyright ===
-# Copyright 2016 Regents of the University of California.
-# All rights reserved.  This software provided pursuant to a
-# license agreement containing restrictions on its disclosure,
-# duplication and use.  For details see:
-# http://www.rbvi.ucsf.edu/chimerax/docs/licensing.html
-# This notice must be embedded in or attached to all copies,
-# including partial copies, of the software or any revisions
-# or derivations thereof.
-# === UCSF ChimeraX Copyright ===
-
-# Python 2 test making an XMLRPC request to run a ChimeraX command
-
 if __name__ == '__main__':
     xmlrpc_port = 42184
 
@@ -23,10 +10,12 @@ if __name__ == '__main__':
     from sys import argv
     #status = s.run_command(argv[1])
     import os
-    arg = argv[1]
+    arg = None
+    if len(argv) > 1:
+        arg = argv[1]
     if arg == 'quit':
         status = s.quit()
-    else:
+    elif arg:
         status = s.load_phenix_refine_final_files(os.path.abspath(os.curdir), arg)
 
     print('Ran "%s", status %s' % (argv[1], status))
