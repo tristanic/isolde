c522d86fa3bfaeafbea2bb90a8d994776d317bad
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2018-12-20 12:43:04 +0000

    Updated restraints __init__.py

--- isolde/src/restraints/__init__.py
+++ isolde/src/restraints/__init__.py
@@ -9,12 +9,40 @@
 
 
 from ..molobject import Restraint_Change_Tracker
-from ..molobject import MDFF_Mgr, Position_Restraint_Mgr, Tuggable_Atoms_Mgr, \
-                        Distance_Restraint_Mgr, Proper_Dihedral_Restraint_Mgr, \
-                        Rotamer_Restraint_Mgr, Chiral_Restraint_Mgr
-from ..molobject import MDFF_Atom, Position_Restraint, Tuggable_Atom, \
-                        Distance_Restraint, Proper_Dihedral_Restraint, \
-                        Rotamer_Restraint, Chiral_Restraint
-from ..molarray import MDFF_Atoms, Position_Restraints, Tuggable_Atoms, \
-                       Distance_Restraints, Proper_Dihedral_Restraints, \
-                       Rotamer_Restraints, Chiral_Restraints
+
+# Manager classes
+from ..molobject import (
+    MDFF_Mgr,
+    Position_Restraint_Mgr,
+    Tuggable_Atoms_Mgr,
+    Distance_Restraint_Mgr,
+    Proper_Dihedral_Restraint_Mgr,
+    Rotamer_Restraint_Mgr,
+    Chiral_Restraint_Mgr
+    )
+#Singular classes
+from ..molobject import (
+    MDFF_Atom,
+    Position_Restraint,
+    Tuggable_Atom,
+    Distance_Restraint,
+    Proper_Dihedral_Restraint,
+    Rotamer_Restraint,
+    Chiral_Restraint
+    )
+# Array classes
+from ..molarray import (
+    MDFF_Atoms,
+    Position_Restraints,
+    Tuggable_Atoms,
+    Distance_Restraints,
+    Proper_Dihedral_Restraints,
+    Rotamer_Restraints,
+    Chiral_Restraints
+    )
+# Utility methods
+from .restraint_utils import (
+    restrain_torsions_to_template,
+    restrain_ca_distances_to_template,
+    restrain_small_ligands
+)
