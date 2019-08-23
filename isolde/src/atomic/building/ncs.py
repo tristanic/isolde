04038c2374c00937c76d387fbe644928be99b607
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2019-08-23 18:46:05 +0100

    Simple method to propagate coordinates across NCS copies

--- isolde/src/atomic/building/ncs.py
+++ isolde/src/atomic/building/ncs.py
@@ -23,3 +23,24 @@ def create_all_ncs_copies(model, transforms):
         if not tf.is_identity():
             for chain in chains:
                 create_ncs_copy(model, chain, tf)
+
+def apply_strict_ncs(template_atoms, ncs_map):
+    '''
+    Copy the transformed coordinates of the template atoms to the given NCS
+    atoms.
+
+    Args:
+
+        * template_atoms: a :class:`Atoms` instance
+        * ncs_map: a list of (:class:`Place`, :class:`Atoms`) pairs. Each
+            array of atoms must have a 1:1 correspondence to the template_atoms.
+            Atoms will be sorted according to (chain, residue number, insertion
+            code, atom name) before applying the NCS operators.
+    '''
+    from chimerax.atomic import Atoms
+    template_atoms = Atoms(sorted(template_atoms, key=lambda a:
+        (a.residue.chain_id, a.residue.number, a.residue.insertion_code, a.name)))
+    for (place, ncs_atoms) in ncs_map:
+        ncs_atoms = Atoms(sorted(ncs_atoms, key=lambda a:
+            (a.residue.chain_id, a.residue.number, a.residue.insertion_code, a.name)))
+        ncs_atoms.coords = place*template_atoms.coords
