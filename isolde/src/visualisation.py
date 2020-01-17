9ad2c8546de6e274ae99f226671798249539f596
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2020-01-17 18:24:04 +0000

    Ensure correct lighting on negative-contour wireframes.

--- isolde/src/visualisation.py
+++ isolde/src/visualisation.py
@@ -2,7 +2,7 @@
 # @Date:   20-Apr-2018
 # @Email:  tic20@cam.ac.uk
 # @Last modified by:   tic20
-# @Last modified time: 26-Apr-2018
+# @Last modified time: 14-Jan-2020
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright:2016-2019 Tristan Croll
 
@@ -31,8 +31,8 @@ human_readable_map_display_styles = {
 # array of settings to apply to the map depending on the chosen
 # representation.
 map_style_settings = {
-    map_styles.mesh_square: {'style': 'mesh', 'square_mesh': True, 'transparency':0},
-    map_styles.mesh_triangle: {'style': 'mesh', 'square_mesh': False, 'transparency':0},
+    map_styles.mesh_square: {'style': 'mesh', 'square_mesh': True, 'transparency':0, 'flip_normals': True},
+    map_styles.mesh_triangle: {'style': 'mesh', 'square_mesh': False, 'transparency':0, 'flip_normals': True},
     map_styles.solid_t20: {'style': 'surface', 'transparency': 0.8},
     map_styles.solid_t40: {'style': 'surface', 'transparency': 0.6},
     map_styles.solid_t60: {'style': 'surface', 'transparency': 0.4},
