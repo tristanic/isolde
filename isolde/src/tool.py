d67159c8b054455307024c49da422bc5847da03a
tic20 <tic20@ketch-linux.cimr.cam.ac.uk>
2020-03-10 11:40:13 +0000

    import MainToolWindow from chimerax.ui not chimerax.core.ui

--- isolde/src/tool.py
+++ isolde/src/tool.py
@@ -2,7 +2,7 @@
 # @Date:   18-Apr-2018
 # @Email:  tic20@cam.ac.uk
 # @Last modified by:   tic20
-# @Last modified time: 12-Dec-2019
+# @Last modified time: 10-Mar-2020
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright:2016-2019 Tristan Croll
 
@@ -34,7 +34,7 @@ class ISOLDE_ToolUI(ToolInstance):
     def __init__(self, session, tool_name):
         ToolInstance.__init__(self, session, tool_name)
         self.display_name = "ISOLDE"
-        from chimerax.core.ui.gui import MainToolWindow
+        from chimerax.ui.gui import MainToolWindow
         self.tool_window = MainToolWindow(self)
         self.tool_window.manage(placement=None)
         parent = self.tool_window.ui_area
