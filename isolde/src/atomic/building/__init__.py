46210baa6313f1d451783be130cafd100c2e43ee
tic20 <tic20@cam.ac.uk>
2020-04-14 20:00:47 +0100

    Set ribbon_hide_backbones = False in merged fragments.

--- isolde/src/atomic/building/__init__.py
+++ isolde/src/atomic/building/__init__.py
@@ -2,7 +2,7 @@
 # @Date:   11-Jun-2019
 # @Email:  tic20@cam.ac.uk
 # @Last modified by:   tic20
-# @Last modified time: 11-Jun-2019
+# @Last modified time: 14-Apr-2020
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright: 2016-2019 Tristan Croll
 
@@ -10,6 +10,7 @@ def set_new_atom_style(session, atoms):
     from chimerax.atomic import selected_atoms, Atom
     from chimerax.core.commands import run
     atoms.draw_modes = Atom.STICK_STYLE
+    atoms.unique_residues.ribbon_hide_backbones=False
     current_sel = selected_atoms(session)
     session.selection.clear()
     atoms.selected = True
