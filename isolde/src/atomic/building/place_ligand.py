c06fb1e1ad4ece543d1b927fa604d2b704e3806e
tic20 <tic20@cam.ac.uk>
2020-04-15 14:07:04 +0100

    Added simple place_water method

--- isolde/src/atomic/building/place_ligand.py
+++ isolde/src/atomic/building/place_ligand.py
@@ -2,7 +2,7 @@
 # @Date:   11-Jun-2019
 # @Email:  tic20@cam.ac.uk
 # @Last modified by:   tic20
-# @Last modified time: 21-Jun-2019
+# @Last modified time: 15-Apr-2020
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright: 2016-2019 Tristan Croll
 
@@ -34,6 +34,21 @@ def place_metal_at_coord(model, chain_id, residue_number, residue_name, atom_nam
     a.coord = coord
     a.bfactor=bfactor
     r.add_atom(a)
+
+def place_water(session, model, position):
+    from chimerax.core.geometry import find_closest_points
+    from chimerax.atomic import mmcif
+    matoms = model.atoms
+    _,_,i = find_closest_points([position], matoms.coords, 3)
+    na = matoms[i[0]]
+    cid = na.residue.chain_id
+    bfactor = na.residue.atoms.bfactors.mean()+5
+    hoh = mmcif.find_template_residue(session, 'HOH')
+    r = new_residue_from_template(model, hoh, cid, position, b_factor=    bfactor)
+    matoms.selected=False
+    r.atoms.selected=True
+    from chimerax.core.commands import run
+    run(session, 'isolde sim start sel')
 
 def new_residue_from_template(model, template, chain_id, center,
         residue_number=None, insert_code=' ', b_factor=50, precedes=None):
