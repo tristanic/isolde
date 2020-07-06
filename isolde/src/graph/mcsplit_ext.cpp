cbc82fedfa5cab03ca7171194f5cc97cabaa6702
tic20 <tic20@cam.ac.uk>
2020-07-06 14:45:29 +0100

    Made McSplit graphs pickleable

--- isolde/src/graph/mcsplit_ext.cpp
+++ isolde/src/graph/mcsplit_ext.cpp
@@ -3,7 +3,7 @@
  * @Date:   17-Jun-2020
  * @Email:  tic20@cam.ac.uk
  * @Last modified by:   tic20
- * @Last modified time: 25-Jun-2020
+ * @Last modified time: 06-Jul-2020
  * @License: Free for non-commercial use (see license.pdf)
  * @Copyright: 2016-2019 Tristan Croll
  */
@@ -81,9 +81,35 @@ PYBIND11_MODULE(mcsplit, m) {
         py::arg("directed")=false,
         py::arg("vertex_labelled")=true,
         py::arg("edge_labelled")=false,
-        py::arg("big_first")=true,
+        py::arg("big_first")=false,
         py::arg("verbose")=false,
         py::arg("quiet")=true
-        );
+        )
+        .def(py::pickle(
+            [](const Graph& g) { // __getstate__
+                std::vector<int> edges1, edges2;
+                size_t i=0;
+                // Adjacency matrix is symmetric, so we only need to iterate
+                // through half of it.
+                for (const auto& column: g.adjmat) {
+                    for (size_t j=0; j<i; j++)
+                        if (column[j] != 0)
+                        {
+                            edges1.push_back(i);
+                            edges2.push_back(j);
+                        }
+                    i++;
+                }
+                return std::tuple<std::vector<unsigned int>, std::vector<int>, std::vector<int>>(g.label, edges1, edges2);
+            },
+            [](std::tuple<std::vector<unsigned int>, std::vector<int>, std::vector<int>> t) {
+                std::vector<unsigned int> labels;
+                std::vector<int> edges1, edges2;
+                std::tie(labels, edges1, edges2) = t;
+                return make_graph(labels, edges1, edges2);
+                //return make_graph(t[0], t[1], t[2]);
+            }
+        ))
+        ;
 
 };
