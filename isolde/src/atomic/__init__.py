a8a8bbb49aa81e218b273b30e6bd258bba4576e8
tic20 <tic20@cam.ac.uk>
2020-07-22 16:10:58 +0100

    Added library of cif templates for core forcefield residues

--- isolde/src/atomic/__init__.py
+++ isolde/src/atomic/__init__.py
@@ -2,7 +2,7 @@
 # @Date:   10-Jul-2018
 # @Email:  tic20@cam.ac.uk
 # @Last modified by:   tic20
-# @Last modified time: 11-Jun-2019
+# @Last modified time: 20-Jul-2020
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright: 2016-2019 Tristan Croll
 
@@ -20,3 +20,15 @@ from ..molobject import ChiralCenter, ProperDihedral, Rama, Rotamer
 
 # Collections
 from ..molarray import ChiralCenters, ProperDihedrals, Ramas, Rotamers
+
+global _templates_loaded
+_templates_loaded = False
+
+def load_default_mmcif_templates():
+    global _templates_loaded
+    if not _templates_loaded:
+        import os
+        from chimerax.atomic import mmcif
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        mmcif.load_mmCIF_templates(os.path.join(base_dir, '..', 'dictionaries', 'core_components.cif'))
+        _templates_loaded=True
