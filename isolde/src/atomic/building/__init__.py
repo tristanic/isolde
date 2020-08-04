f37eb0f1b8fce6ac1f472353a4d937fbb503e5f9
tic20 <tic20@cam.ac.uk>
2020-08-04 18:02:47 +0100

    set_default_atom_style() sets ribbon display

--- isolde/src/atomic/building/__init__.py
+++ isolde/src/atomic/building/__init__.py
@@ -2,7 +2,7 @@
 # @Date:   11-Jun-2019
 # @Email:  tic20@cam.ac.uk
 # @Last modified by:   tic20
-# @Last modified time: 14-Apr-2020
+# @Last modified time: 03-Aug-2020
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright: 2016-2019 Tristan Croll
 
@@ -10,7 +10,9 @@ def set_new_atom_style(session, atoms):
     from chimerax.atomic import selected_atoms, Atom
     from chimerax.core.commands import run
     atoms.draw_modes = Atom.STICK_STYLE
-    atoms.unique_residues.ribbon_hide_backbones=False
+    residues = atoms.unique_residues
+    residues.ribbon_displays=True
+    residues.ribbon_hide_backbones=False
     current_sel = selected_atoms(session)
     session.selection.clear()
     atoms.selected = True
