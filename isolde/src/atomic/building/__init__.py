728d184918eb31b60f09ae7b05d959a500c24baa
tic20 <tic20@cam.ac.uk>
2021-05-13 13:24:46 +0100

    Improved behaviour of add_amino_acid_residue

--- isolde/src/atomic/building/__init__.py
+++ isolde/src/atomic/building/__init__.py
@@ -6,16 +6,23 @@
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright: 2016-2019 Tristan Croll
 
-def set_new_atom_style(session, atoms):
+def copy_atom_style_from(session, atoms, ref_residue):
     from chimerax.atomic import selected_atoms, Atom
     from chimerax.core.commands import run
-    atoms.draw_modes = Atom.STICK_STYLE
+    r = ref_residue
+    atoms.draw_modes = r.atoms[0].draw_mode
     residues = atoms.unique_residues
-    residues.ribbon_displays=True
-    residues.ribbon_hide_backbones=False
+    residues.ribbon_displays=r.ribbon_display
+    residues.ribbon_hide_backbones=r.ribbon_hide_backbone
     current_sel = selected_atoms(session)
     session.selection.clear()
     atoms.selected = True
-    run(session, "color sel bychain; color sel byhetero", log=False)
+    ref_c = r.atoms[r.atoms.element_names=='C']
+    if len(ref_c):
+        atoms[atoms.element_names=='C'].colors = ref_c[0].color
+    else:
+        run(session, "color sel bychain", log=False)
+    run(session, "color sel byhetero", log=False)
     session.selection.clear()
     current_sel.selected = True
+
