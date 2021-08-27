73046ca9767ce1150aeab8157605322740a04db3
tic20 <tic20@cam.ac.uk>
2021-08-27 18:26:53 +0100

    Updates to AlphaFold domain clustering

--- isolde/src/reference_model/alphafold/__init__.py
+++ isolde/src/reference_model/alphafold/__init__.py
@@ -1,19 +1,30 @@
 
-def fetch_alphafold_pae(session, uniprot_id, ignore_cache=False):
+_pae_cache = dict()
+
+def fetch_alphafold_pae(session, uniprot_id, ignore_matrix_cache=False, ignore_download_cache=False):
     '''
     Fetch the predicted aligned error matrix for an AlphaFold prediction from the 
     EMBL AlphaFold server.
     '''
+    global _pae_cache
+    if not ignore_matrix_cache:
+        pae = _pae_cache.get(uniprot_id, None)
+        if pae is not None:
+            return pae
     from chimerax.core.fetch import fetch_file
     file_name = f'AF-{uniprot_id}-F1-predicted_aligned_error_v1.json'
     url = f'https://alphafold.ebi.ac.uk/files/{file_name}'
 
     filename = fetch_file(session, url, f'Alphafold {uniprot_id} PAE', file_name, 'AlphaFold-PAE',
-        ignore_cache=ignore_cache)
+        ignore_cache=ignore_download_cache)
     
+    pae = _pae_cache[uniprot_id] = parse_pae_file(filename)
+    return pae
+
+def parse_pae_file(pae_json_file):
     import json, numpy
 
-    with open(filename, 'rt') as f:
+    with open(pae_json_file, 'rt') as f:
         data = json.load(f)[0]
     
     r1, r2, d = data['residue1'],data['residue2'],data['distance']
@@ -26,3 +37,21 @@ def fetch_alphafold_pae(session, uniprot_id, ignore_cache=False):
 
     return matrix
 
+
+def alphafold_id(model):
+    metadata = model.metadata
+    entry_keys = metadata.get('entry', None)
+    if entry_keys is None:
+        return None
+    entry_data = metadata.get('entry data', None)
+    if entry_data is None:
+        return None
+    try:
+        id_index = entry_keys.index('id')
+    except ValueError:
+        return None
+    import re
+    result = re.match('AF\-[A-Z0-9]*\-F1', entry_data[id_index-1])
+    if result is None:
+        return None
+    return result[0]
--- isolde/src/reference_model/alphafold/find_domains.py
+++ isolde/src/reference_model/alphafold/find_domains.py
@@ -1,30 +1,106 @@
 
-def cluster_into_domains(model, pae_matrix, distance_cutoff=5, graph_resolution=1, color_by_cluster=True):
+def cluster_into_domains(session, model, pae_matrix, pae_power=1, pae_cutoff=5, adjust_weights_for_distance=True, distance_power=1, graph_resolution=1, color_by_cluster=True):
     import networkx as nx
     import numpy
-    weights = (1/pae_matrix*10000).astype(numpy.int)
+    weights = 1/pae_matrix**pae_power
+    if adjust_weights_for_distance:
+        distances = distance_matrix(model, num_residues = pae_matrix.shape[0])
+        weights = weights/distances**distance_power
+    # Within the NetworkX greedy_modularity_communities() method the weight is used to define a term
+    # that becomes one element of a dict key. Therefore the use of floating point is inadvisable (can 
+    # lead to random `KeyError`s). So, we convert the weights to integers, first multiplying by a 
+    # sufficiently large number to make sure everything is left of the decimal place.
+    # Note that as of 19 August 2021 `KeyError`s can still happen - this has been reported to the
+    # NetworkX developers (https://github.com/networkx/networkx/issues/4992) and possible fixes are being 
+    # explored.
+    #weights = (weights * 1e6).astype(numpy.int)
+
     g = nx.Graph()
     size = weights.shape[0]
     g.add_nodes_from(range(size))
     for i in range(size):
-        for j in range(i, size):
+        for j in range(i+1, size):
             pae = pae_matrix[i,j]
-            if pae < distance_cutoff:
-                g.add_edge(i, j, weight=weights[i,j])
+            if pae < pae_cutoff:
+                weight = weights[i,j]
+                if weight > 0:
+                    g.add_edge(i, j, weight=weight)
 
     from networkx.algorithms import community
 
     clusters = community.greedy_modularity_communities(g, weight='weight', resolution=graph_resolution)
     residue_clusters = []
     for i, c in enumerate(clusters):
-        residues = model.residues[numpy.array(list(c))]
+        residues = model.residues[numpy.in1d(model.residues.numbers,[r+1 for r in c])]
         residue_clusters.append(residues)
         for r in residues:
             r.isolde_domain = i
     
     if color_by_cluster:
         from chimerax.core.commands import run
-        run(session, f'color byattribute r:isolde_domain #{model.id_string} target ra palette rainbow')
+        run(session, f'color byattribute r:isolde_domain #{model.id_string} target ra palette Paired-12')
 
     return residue_clusters
 
+def distance_matrix(m, num_residues = None):
+    if num_residues is None:
+        num_residues = max(m.residues.numbers)
+    from chimerax.geometry import distance
+    import numpy
+    size = num_residues
+    distances = numpy.ones([size,size])
+    cas = m.atoms[m.atoms.names=='CA']
+    for i, ca1 in enumerate(cas):
+        for ca2 in cas[i+1:]:
+            dist = distance(ca1.coord, ca2.coord)
+            r1num = ca1.residue.number
+            r2num = ca2.residue.number
+            distances[r1num-1,r2num-1] = dist
+            distances[r2num-1,r1num-1] = dist
+    return distances
+
+
+def domains_from_pae_matrix(pae_matrix, pae_power=1, pae_cutoff=5, graph_resolution=1):
+    '''
+    Takes a predicted aligned error (PAE) matrix representing the predicted error in distances between each 
+    pair of residues in a model, and uses a graph-based community clustering algorithm to partition the model
+    into approximately rigid groups.
+
+    Arguments:
+
+        * pae_matrix: a (n_residues x n_residues) numpy array. Diagonal elements should be set to some non-zero
+          value to avoid divide-by-zero warnings
+        * pae_power (optional, default=1): each edge in the graph will be weighted proportional to (1/pae**pae_power)
+        * pae_cutoff (optional, default=5): graph edges will only be created for residue pairs with pae<pae_cutoff
+        * graph_resolution (optional, default=1): regulates how aggressively the clustering algorithm is. Smaller values
+          lead to larger clusters. Value should be larger than zero, and values larger than 5 are unlikely to be useful.
+
+    Returns: a series of lists, where each list contains the indices of residues belonging to one cluster.
+    '''
+    import networkx as nx
+    import numpy
+    weights = 1/pae_matrix**pae_power
+    # Within the NetworkX greedy_modularity_communities() method the weight is used to define a term
+    # that becomes one element of a dict key. Therefore the use of floating point is inadvisable (can 
+    # lead to random `KeyError`s). So, we convert the weights to integers, first multiplying by a 
+    # sufficiently large number to make sure everything is left of the decimal place.
+    # Note that as of 19 August 2021 `KeyError`s can still happen - this has been reported to the
+    # NetworkX developers (https://github.com/networkx/networkx/issues/4992) and possible fixes are being 
+    # explored.
+    weights = (weights * 1e6).astype(numpy.int)
+
+    g = nx.Graph()
+    size = weights.shape[0]
+    g.add_nodes_from(range(size))
+    for i in range(size):
+        for j in range(i+1, size):
+            pae = pae_matrix[i,j]
+            if pae < pae_cutoff:
+                weight = weights[i,j]
+                if weight > 0:
+                    g.add_edge(i, j, weight=weight)
+
+    from networkx.algorithms import community
+
+    clusters = community.greedy_modularity_communities(g, weight='weight', resolution=graph_resolution)
+    return clusters
