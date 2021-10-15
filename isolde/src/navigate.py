d051decf01794c57b0978da1517d37d6c8d18cf9
tic20 <tic20@cam.ac.uk>
2021-10-15 14:23:27 +0100

    Only use field width when camera is orthographic

--- isolde/src/navigate.py
+++ isolde/src/navigate.py
@@ -196,7 +196,8 @@ class ResidueStepper(StateManager):
         tc = self._camera_ref_pos(self.view_distance)
         np = p*tc
         new_cofr = centroid
-        fw = c.field_width
+        if c.name=='orthographic':
+            fw = c.field_width
         new_fw = self._view_distance*2
 
         def interpolate_camera(session, f, cp=cp, np=np, oc=old_cofr, nc=new_cofr, fw=fw, nfw=new_fw, vr=self._view_distance, center=np.inverse()*centroid, frames=self._interpolate_frames):
