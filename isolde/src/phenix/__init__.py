b1f0c8d7e1ed58f171707810c6a244a2e4dc8faa
tic20 <tic20@cam.ac.uk>
2021-11-17 11:41:48 +0000

    Find and store path to Phenix

--- isolde/src/phenix/__init__.py
+++ isolde/src/phenix/__init__.py
@@ -8,14 +8,22 @@
 
 DEFAULT_MIN_PHENIX_VERSION=3964
 
-def check_for_phenix_version():
+
+def check_for_phenix_version(session):
     from chimerax.core.errors import UserError
     import sys, os, subprocess
-    cmd_args = ["phenix.version"]
+    from ..settings import basic_settings
+    phenix_path = basic_settings.phenix_base_path
+    if phenix_path is None:
+        phenix_path = _choose_phenix_directory(session)
+        basic_settings.phenix_base_path = phenix_path
+    cmd_args = [os.path.join(phenix_path, "phenix.version")]
     try:
         pipes = subprocess.Popen(cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     except FileNotFoundError:
-        raise UserError('The Phenix plugin requires Phenix to be installed and on your execution path!')
+        session.logger.warning('Phenix installation appears to have moved or been deleted. Please provide a new path.')
+        basic_settings.phenix_base_path = None
+        return check_for_phenix_version(session)
     std_out, std_err = pipes.communicate()
     lines = [line.strip() for line in std_out.decode('utf-8').split('\n')]
     version = _parse_version(lines)
@@ -30,6 +38,28 @@ def _parse_version(lines):
 
 
 
-def check_for_phenix():
-    version = check_for_phenix_version()
+def check_for_phenix(session):
+    version = check_for_phenix_version(session)
     return version >= DEFAULT_MIN_PHENIX_VERSION
+
+def _choose_phenix_directory(session):
+    satisfied = False
+    from Qt.QtWidgets import QFileDialog, QMessageBox
+    parent = session.ui.main_window
+    import subprocess, os
+    while not satisfied:
+        result = QFileDialog.getExistingDirectory(parent, 'Please provide the directory containing the Phenix executables.', options=QFileDialog.Options())
+        if not result:
+            break
+        try:
+            subprocess.call([os.path.join(result,'phenix.version')])
+            satisfied = True
+        except FileNotFoundError:
+            choice = QMessageBox.warning(parent, 'This directory does not appear to contain Phenix executables. Would you like to try again?',
+                QMessageBox.Ok|QMessageBox.Cancel)
+        except:
+            raise
+    if not satisfied:
+        from chimerax.core.errors import UserError
+        raise UserError('Could not find Phenix installation. Operation cancelled')
+    return result
--- isolde/src/phenix/parameterise.py
+++ isolde/src/phenix/parameterise.py
@@ -6,76 +6,6 @@
 # @License: Free for non-commercial use (see license.pdf)
 # @Copyright: 2016-2019 Tristan Croll
 
-def parameterise_residue_with_elbow(residue, include_hydrogens=True, keep_elbow_files=True):
-    session = residue.session
-    resname = residue.name
-    from chimerax.core.errors import UserError
-    from chimerax import mmcif
-    from . import check_for_phenix
-    from chimerax.isolde.parmed import install_parmed_if_necessary
-    install_parmed_if_necessary(session)
-    if not check_for_phenix():
-        raise UserError('Automatic MD parameterisaton requires an up-to-date version of the Phenix suite (https://www.phenix-online.org)')
-    if len(residue.neighbors):
-        raise UserError('Automatic MD parameterisation is currently only supported for free, non-covalently bound ligands.')
-    if any(residue.atoms.elements.is_metal):
-        raise UserError('Automatic MD parameterisation is not yet supported for metal complexes.')
-
-    from ..cmd import isolde_start
-    isolde_start(session)
-    check_if_params_exist(session, residue)
-
-    import os
-    cwd = os.getcwd()
-    import tempfile
-    tmpdir = tempfile.TemporaryDirectory()
-    tmpdirname = tmpdir.name
-    os.chdir(tmpdirname)
-
-    from chimerax.mol2 import write_mol2
-    session.logger.status('Running eLBOW. This may take a while...')
-    filename = resname + '_for_elbow.mol2'
-    atoms = residue.atoms
-    if not include_hydrogens:
-        atoms = atoms[atoms.element_names != 'H']
-    try:
-        write_mol2(session, filename, atoms=residue.atoms)
-        _run_elbow(session, filename)
-    except:
-        os.chdir(cwd)
-        raise
-    expected_files = (resname+'.frcmod', resname+'.mol2', resname+'.cif')
-    if not all(os.path.isfile(efile) for efile in expected_files):
-        os.chdir(cwd)
-        raise UserError('phenix.elbow finished without an error code, but the expected files were not written. Bailing out.')
-    from chimerax.isolde.openmm.amberff.amber_convert import amber_to_ffxml
-    amber_to_ffxml(*expected_files)
-    xmlfile = resname+'.xml'
-    if not os.path.isfile(xmlfile):
-        os.chdir(cwd)
-        raise UserError('Failed to convert .frcmod/.mol2 to ffXML.')
-    import shutil
-    if keep_elbow_files:
-        import glob
-        all_files = glob.glob('*')
-        for f in all_files:
-            shutil.copy(os.path.join(tmpdirname, f), os.path.join(cwd, f))
-            shutil.move(os.path.join(cwd, resname+'.cif'), os.path.join(cwd, resname+'_restraints.cif'))
-    else:
-        final_loc = os.path.join(cwd, xmlfile)
-        shutil.copy(os.path.join(tmpdirname, xmlfile), final_loc)
-        shutil.copy(os.path.join(tmpdirname, resname+'.cif'), os.path.join(cwd, resname+'_restraints.cif'))
-    os.chdir(cwd)
-    ff_name = session.isolde.sim_params.forcefield
-    ff = session.isolde.forcefield_mgr[ff_name]
-    session.isolde.add_ffxml_files(ff, [xmlfile])
-
-    info_str = ('Wrote AMBER parameters for residue {} to {}. These have been '
-        'loaded into your forcefield for this session. For future ISOLDE sessions, '
-        'load this file using the "Load residue MD definition(s)" button on '
-        'the ISOLDE panel.').format(resname, final_loc)
-    session.logger.info(info_str)
-
 def check_if_params_exist(session, residue):
     from chimerax.atomic import Residues
     from chimerax.core.errors import UserError
@@ -103,17 +33,84 @@ def check_if_params_exist(session, residue):
         err_str = err_str_base.format(residue.name, assigned[r][0].name)
         raise UserError(err_str)
 
+def parameterise_residue_with_elbow(residue, include_hydrogens=True, keep_elbow_files=False):
+    session = residue.session
+    resname = residue.name
+    from chimerax.core.errors import UserError
+    from chimerax import mmcif
+    from . import check_for_phenix
+    from chimerax.isolde.parmed import install_parmed_if_necessary
+    install_parmed_if_necessary(session)
+    if not check_for_phenix(session):
+        raise UserError('Automatic MD parameterisaton requires an up-to-date version of the Phenix suite (https://www.phenix-online.org)')
+    if len(residue.neighbors):
+        raise UserError('Automatic MD parameterisation is currently only supported for free, non-covalently bound ligands.')
+    if any(residue.atoms.elements.is_metal):
+        raise UserError('Automatic MD parameterisation is not yet supported for metal complexes.')
+
+    from ..cmd import isolde_start
+    isolde_start(session)
+    check_if_params_exist(session, residue)
+
+    import os
+    cwd = os.getcwd()
+    from ..util import SafeTempDir
+    with SafeTempDir():
+        session.logger.status('Running eLBOW. This may take a while...')
+        filename = resname + '_for_elbow.pdb'
+        atoms = residue.atoms
+        if not include_hydrogens:
+            atoms = atoms[atoms.element_names != 'H']
+        from chimerax.pdb import save_pdb
+        session.selection.clear()
+        atoms.selected=True
+        save_pdb(session, filename, selected_only=True)
+        _run_elbow(session, filename)
+        expected_files = (resname+'.frcmod', resname+'.mol2')
+        if not all(os.path.isfile(efile) for efile in expected_files):
+            raise UserError('phenix.elbow finished without an error code, but the expected files were not written. Bailing out.')
+        from chimerax.isolde.openmm.amberff.amber_convert import amber_to_ffxml
+        amber_to_ffxml(*expected_files)
+        xmlfile = resname+'.xml'
+        if not os.path.isfile(xmlfile):
+            raise UserError('Failed to convert .frcmod/.mol2 to ffXML.')
+        import shutil
+        if keep_elbow_files:
+            import glob
+            all_files = glob.glob('*')
+            for f in all_files:
+                shutil.copy(f, os.path.join(cwd, f))
+        else:
+            final_loc = os.path.join(cwd, xmlfile)
+            shutil.copy(xmlfile, final_loc)
+    ff_name = session.isolde.sim_params.forcefield
+    ff = session.isolde.forcefield_mgr[ff_name]
+    session.isolde.add_ffxml_files(ff, [xmlfile])
+
+    info_str = ('Wrote AMBER parameters for residue {} to {}. These have been '
+        'loaded into your forcefield for this session. For future ISOLDE sessions, '
+        'load this file using the "Load residue MD definition(s)" button on '
+        'the ISOLDE panel.').format(resname, final_loc)
+    session.logger.info(info_str)
 
 
 
 
-def _run_elbow(session, mol2_file):
-    import sys, os, subprocess
-    cmd_args = ["phenix.elbow", "--amber", mol2_file]
-    pipes = subprocess.Popen(cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def _run_elbow(session, coord_file):
+    from ..settings import basic_settings
+    phenix_path = basic_settings.phenix_base_path
+    import os, subprocess
+    cmd_args = [os.path.join(phenix_path, "phenix.elbow"), "--amber", coord_file]
+    pipes = subprocess.Popen(cmd_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
+        creationflags=subprocess.CREATE_NEW_CONSOLE)
     std_out, std_err = pipes.communicate()
 
     if pipes.returncode !=0:
         from chimerax.core.errors import UserError
-        err_str = "Attempt to run phenix.elbow failed with the following error: \n{}".format(std_err.strip().decode('utf-8'))
+        err_str = (f"Attempt to run phenix.elbow failed with the following error: \n{std_err.strip().decode('utf-8')}\n\n"
+            'This could be due to poor geometry in your input '
+            'coordinates, or chemistry that eLBOW does not understand (it often fails on charged groups). At '
+            'present ISOLDE cannot help with this - you may need to install and run ANTECHAMBER for yourself.')
         raise UserError(err_str)
