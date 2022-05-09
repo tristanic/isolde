efbe3cd9c0d96b58d5f338c371534b255e04fe8a
Tristan Croll <tic20@cam.ac.uk>
2022-05-09 08:43:18 +0100

    Add "centre on selection" option

--- isolde/src/ui/general_tab/mask_settings.py
+++ isolde/src/ui/general_tab/mask_settings.py
@@ -2,12 +2,12 @@ from ..util import slot_disconnected
 from ..ui_base import (
     DefaultSpacerItem,
     UI_Panel_Base,
-    DefaultHLayout
+    DefaultHLayout, DefaultVLayout
 )
 
 from ..collapse_button import CollapsibleArea
 
-from Qt.QtWidgets import QLabel, QDoubleSpinBox
+from Qt.QtWidgets import QLabel, QDoubleSpinBox, QCheckBox
 
 class MaskAndSpotlightSettingsPanel(CollapsibleArea):
     def __init__(self, session, isolde, parent, gui, **kwargs):
@@ -23,9 +23,10 @@ class MaskAndSpotlightSettingsDialog(UI_Panel_Base):
 
         params = self.isolde.params
         mf = self.main_frame
-        ml = self.main_layout = DefaultHLayout()
+        ml = self.main_layout = DefaultVLayout()
+        l1 = DefaultHLayout()
 
-        ml.addWidget(QLabel('Mask radius (Å): ', parent=mf))
+        l1.addWidget(QLabel('Mask radius (Å): ', parent=mf))
         mrsb = self.mask_radius_spinbox = QDoubleSpinBox(mf)
         mrsb.setValue(params.map_mask_radius)
         mrsb.setDecimals(1)
@@ -33,11 +34,11 @@ class MaskAndSpotlightSettingsDialog(UI_Panel_Base):
         mrsb.setMinimum(0.5)
         mrsb.setMaximum(100)
         mrsb.valueChanged.connect(self._mask_radius_changed_cb)
-        ml.addWidget(mrsb)
+        l1.addWidget(mrsb)
 
-        ml.addItem(DefaultSpacerItem())
+        l1.addItem(DefaultSpacerItem())
 
-        ml.addWidget(QLabel('Spotlight radius (Å): ', parent=mf))
+        l1.addWidget(QLabel('Spotlight radius (Å): ', parent=mf))
         slsb = self.spotlight_radius_spinbox = QDoubleSpinBox(mf)
         slsb.setValue(params.spotlight_radius)
         slsb.setDecimals(1)
@@ -45,7 +46,17 @@ class MaskAndSpotlightSettingsDialog(UI_Panel_Base):
         slsb.setMinimum(5)
         slsb.setMaximum(1000)
         slsb.valueChanged.connect(self._spotlight_radius_spinbox_changed_cb)
-        ml.addWidget(slsb)
+        l1.addWidget(slsb)
+        ml.addLayout(l1)
+
+        l2 = DefaultHLayout()
+        fcb = self.center_on_selection_checkbox = QCheckBox('Centre on selection when masking', parent=mf)
+        fcb.setChecked(isolde.params.center_on_sel_when_masking)
+        def _cb(state):
+            isolde.params.center_on_sel_when_masking = state
+        fcb.stateChanged.connect(_cb)
+        l2.addWidget(fcb)
+        ml.addLayout(l2)
 
         self._selected_model_changed_cb('', self.isolde.selected_model)
 
