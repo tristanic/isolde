200eae46b39bc10801a00e9899de6db4e676f69d
Tristan Croll <tcroll@altoslabs.com>
2025-05-12 16:20:48 +0100

    Correctly provide OpenMM lib and include paths in Win

--- isolde/prep_toml.py
+++ isolde/prep_toml.py
@@ -1,6 +1,9 @@
-import openmm
+import openmm, os
 lib_dir = openmm.version.openmm_library_path
+include_dir = os.path.abspath(os.path.join(lib_dir, '..', 'include'))
 with open("pyproject.toml.in", "rt") as f:
-    output = f.read().replace("OPENMM_LIB_DIR", lib_dir)
+    output = f.read()\
+        .replace("OPENMM_LIB_DIR", repr(lib_dir).replace("'",""))\
+        .replace("OPENMM_INCLUDE_DIR", repr(include_dir).replace("'",""))
 with open("pyproject.toml", "wt") as f:
     f.write(output)
